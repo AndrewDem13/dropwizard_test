07454fa7a809e122b4bc41753a05a291ae6aa08a
Andrii Demyanenko <ademi@softserveinc.com>
2017-08-29 17:59:50 +0300

    Embeded Mongo added to integration tests

--- src/test/java/integration/IntegrationTest.java
+++ src/test/java/integration/IntegrationTest.java
@@ -3,12 +3,16 @@ package integration;
 import com.softserveinc.dropwizard_test.App;
 import com.softserveinc.dropwizard_test.AppConfiguration;
 import com.softserveinc.dropwizard_test.entity.Entity;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
-import org.junit.Assert;
-import org.junit.ClassRule;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runners.MethodSorters;
 
 import javax.ws.rs.core.GenericType;
@@ -20,26 +24,42 @@ public class IntegrationTest {
 
     private static final String CONFIG_PATH = ResourceHelpers.resourceFilePath("test-config.yml");
 
-
     @ClassRule
     public static final DropwizardAppRule<AppConfiguration> RULE = new DropwizardAppRule<>(App.class, CONFIG_PATH);
 
+    private static MongodExecutable mongodExe;
+    private static MongodProcess mongod;
+
     private final Entity entity = new Entity(1, "test");
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        MongodStarter runtime = MongodStarter.getDefaultInstance();
+        mongodExe = runtime
+                    .prepare(new MongodConfigBuilder()
+                            .version(Version.Main.PRODUCTION)
+                            .net(new Net(RULE.getConfiguration().mongohost, RULE.getConfiguration().mongoport, Network.localhostIsIPv6()))
+                            .build());
+        mongod = mongodExe.start();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        if (mongod != null) {
+            mongod.stop();
+            mongodExe.stop();
+        }
+    }
+
 
     @Test
     public void _01create() {
-        Response response = RULE.client()
-                .target(String.format(("http://localhost:%d/entity/"), RULE.getLocalPort()))
+        Entity newEntity = RULE.client()
+                .target(String.format(("http://localhost:%d/entity"), RULE.getLocalPort()))
                 .request()
-                .post(javax.ws.rs.client.Entity.json(entity));
-
-//        Entity newEntity = RULE.client()
-//                .target(String.format(("http://localhost:%d/entity"), RULE.getLocalPort()))
-//                .request()
-//                .post(javax.ws.rs.client.Entity.json(entity))
-//                .readEntity(Entity.class);
-
-        Assert.assertEquals(201, response.getStatus());
+                .post(javax.ws.rs.client.Entity.json(entity))
+                .readEntity(Entity.class);
+        Assert.assertEquals(newEntity, entity);
     }
 
     @Test
--- src/test/java/integration/OkHttpClientTest.java
+++ src/test/java/integration/OkHttpClientTest.java
@@ -5,13 +5,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.softserveinc.dropwizard_test.App;
 import com.softserveinc.dropwizard_test.AppConfiguration;
 import com.softserveinc.dropwizard_test.entity.Entity;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import okhttp3.*;
-import org.junit.Assert;
-import org.junit.ClassRule;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runners.MethodSorters;
 
 import java.util.List;
@@ -26,8 +30,30 @@ public class OkHttpClientTest {
     @ClassRule
     public static final DropwizardAppRule<AppConfiguration> RULE = new DropwizardAppRule<>(App.class, CONFIG_PATH);
 
+    private static MongodExecutable mongodExe;
+    private static MongodProcess mongod;
+
     private final String URL = String.format(("http://localhost:%d/entity/"), RULE.getLocalPort());
     private final Entity entity = new Entity(1, "test");
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        MongodStarter runtime = MongodStarter.getDefaultInstance();
+        mongodExe = runtime
+                .prepare(new MongodConfigBuilder()
+                        .version(Version.Main.PRODUCTION)
+                        .net(new Net(RULE.getConfiguration().mongohost, RULE.getConfiguration().mongoport, Network.localhostIsIPv6()))
+                        .build());
+        mongod = mongodExe.start();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        if (mongod != null) {
+            mongod.stop();
+            mongodExe.stop();
+        }
+    }
 
     @Test
     public void _01create() throws Exception {
